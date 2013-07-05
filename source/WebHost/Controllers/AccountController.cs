6bed42cab6ad74b7ab0bfa4cdfe97c39a3dbddf1
Poul Sørensen <poulmobil@gmail.com>
2013-07-05 04:52:37 +0200

    Updated to my IdPs

--- source/WebHost/Controllers/AccountController.cs
+++ source/WebHost/Controllers/AccountController.cs
@@ -4,17 +4,29 @@
  */
 
 using System.IdentityModel.Services;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace Thinktecture.AuthorizationServer.WebHost.Controllers
 {
     public class AccountController : Controller
     {
-        public ActionResult SignOut()
+        public async Task<ActionResult> SignOut()
         {
-            if (User.Identity.IsAuthenticated)
+            if (User.Identity.IsAuthenticated)            
             {
-                FederatedAuthentication.WSFederationAuthenticationModule.SignOut();
+                var authModule = FederatedAuthentication.WSFederationAuthenticationModule;
+                var url = WSFederationAuthenticationModule.GetFederationPassiveSignOutUrl(authModule.Issuer, authModule.Realm, null);
+                var request = WebRequest.Create(url);
+                var task = request.GetResponseAsync();
+
+                authModule.SignOut();
+
+
+                var result = await task;
+                
+                
             }
             return View();
         }
